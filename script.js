b63c7f5452721d838414f18833d67f5e93ee3d8e
HM\STBAK <stefan.barkelius@hm.com>
2020-10-06 14:30:41 +0200

    Added popup features in external links menu, also added some scripts in scripts.js

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+//Stefans changes below
+
+
+function showMe(caller) {
+  var txtWWF = "Världsnaturfonden WWF (engelska: World Wide Fund for Nature, ursprungligen World Wildlife Fund) är en internationell organisation inom naturvård. Den bildades 1961 och är en partipolitiskt obunden ideell organisation. Internationellt sett är den en av de största privata miljö- och naturvårdsorganisationerna. Den har sitt huvudkontor i Gland i Schweiz.";
+  var txtLS = "For the science geek in everyone, Live Science breaks down the stories behind the most interesting news and photos on the Internet, while also digging up fascinating discoveries that hit on a broad range of fields, from dinosaurs and archaeology to wacky physics and astronomy to health and human behavior. If you want to learn something interesting every day, Live Science is the place for you."
+  var txtNG = "National Geographic Channel är tv-kanalen från mediakoncernerna National Geographic Society och Rupert Murdochs mediekoncern News Corporation. Den förstnämnda är mest känd för tidningen med samma namn.";
+  document.getElementById("hi").style.visibility="Visible";
+  if (caller == "wwf") {
+    txt = txtWWF;
+  } else if (caller == "LS") {
+    txt = txtLS;
+  } else if (caller == "NG") {
+    txt = txtNG;
+  }
+  document.getElementById("hi").innerText=txt;
+}
+
+function hideMe() {
+  document.getElementById("hi").style.visibility="hidden";
+}
